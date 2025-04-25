42e998d4743bee6a99c970078b4d7f2a50cd96c4
Agnesnti <agnes.westerdahlmansson@elev.ga.ntig.se>
2025-04-25 15:19:12 +0200

    komplettering
    Jag har lagt till kommentarer i koden. Ändrat div till button. Jag har lagt till S, M och L som 3 olika buttons. Jag har lagt till lite information om varje produkt för att göra sidan mer verklighetstrogen.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
+/* Vänta tills hela HTML-dokumentet har laddats innan koden körs */
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("hamburger");
     const navMenu = document.getElementById("nav-menu");
 
+    /* Om hamburgermenyn finns, lyssna på klick*/
     if (hamburger) {
         hamburger.addEventListener("click", function () {
+            /* Visa eller göm menyn när man klickar*/
             navMenu.classList.toggle("active");
         });
     }
